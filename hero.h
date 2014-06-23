5df2557e044c3793dcee4044529e94ca6f307108
Zach-Kauffman <hyperion9823@gmail.com>
2014-06-22 19:25:50 -0700

    Update hero.h

--- hero.h
+++ hero.h
@@ -1,4 +1,6 @@
-class hero
+class hero: public melee, public staff
+//allows us to make use of variables/functions from other classes
+//for instance, we can use the damage stat from the melee class in the attack function of hero
 {
 public:
   int hitpoints;
@@ -13,6 +15,4 @@ public:
   void rest();
   void run();
   void consume();
-  
-  
 };
