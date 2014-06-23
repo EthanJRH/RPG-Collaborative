e9c696550c1dac22ef72b6c804d2ff93e6134085
Zach-Kauffman <hyperion9823@gmail.com>
2014-06-22 19:28:56 -0700

    Update melee.h

--- melee.h
+++ melee.h
@@ -5,7 +5,8 @@ public:
   int damageMultilpy;
   int speedMultiply;
   int crit;
-  
-  void attack();
+  //void attack();
+  //attack function is on the hero class
+  //we need to standardize what functions go where
   void specAttack();
 };
