bdedb8f074cceb43017becbd63b6b84297d8a36a
Zach-Kauffman <hyperion9823@gmail.com>
2014-06-22 20:09:15 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,19 +7,4 @@
 using namespace std;
 int main()
 {
-    string entry;
-    cout << "welcome to the game\n1: Start\t2: Help\t\t3: Quit" << endl;
-    cin >> entry;
-    if (entry == "1") 
-    {
-        game.start();
-    } else if (entry = "2") 
-    {
-        cout << "someone make a help screen thanks guys\n\n";
-        main();
-    } else 
-    {
-        return 0;
-    }
-    return 0;
 }
