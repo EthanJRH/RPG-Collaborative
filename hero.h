a423b73832e5d5d288d4a5379cef2e276ab4eb67
BadassHebrew <eugenem98@hotmail.com>
2014-06-22 18:16:59 -0700

    Update hero.h

--- hero.h
+++ hero.h
@@ -3,6 +3,16 @@ class hero
 public:
   int hitpoints;
   int mana;
+  int speed;
+  int attack;
+  int magic;
+  int crit;
   bool male;
+  void attack();
+  void ability();
+  void rest();
+  void run();
+  void consume();
+  
   
 };
